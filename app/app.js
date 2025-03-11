83f430fb80ea27649df6b6686eeea9a22f6c7985
moiz202 <moizamir202@gmail.com>
2025-03-11 11:21:37 +0000

    fixing scaffholding files

--- app/app.js
+++ app/app.js
@@ -7,6 +7,10 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
@@ -36,105 +40,6 @@ app.get("/students", async function(req, res) {
     }
 });
 
-// Task 2: Provide HTML formatted output for all students
-app.get("/students", async function(req, res) {
-    try {
-        const students = await db.query("SELECT * FROM Students");
-        let html = `
-            <h1>List of Students</h1>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>`;
-        
-        students.forEach(student => {
-            html += `
-                <tr>
-                    <td>${student.id}</td>
-                    <td><a href="/students/${student.id}">${student.name}</a></td>
-                </tr>`;
-        });
-        
-        html += `</tbody></table>`;
-        res.send(html); // Send the HTML table
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error fetching students data.");
-    }
-});
-
-// Task 3: Provide JSON output of all programmes
-app.get("/programmes", async function(req, res) {
-    try {
-        const programmes = await db.query("SELECT * FROM Programmes");
-        res.json(programmes); // Send the programmes as JSON response
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error fetching programmes data.");
-    }
-});
-
-// Task 4: Provide HTML formatted output of all programmes
-app.get("/programmes", async function(req, res) {
-    try {
-        const programmes = await db.query("SELECT * FROM Programmes");
-        let html = `
-            <h1>List of Programmes</h1>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>`;
-        
-        programmes.forEach(programme => {
-            html += `
-                <tr>
-                    <td>${programme.id}</td>
-                    <td><a href="/programmes/${programme.id}">${programme.name}</a></td>
-                </tr>`;
-        });
-        
-        html += `</tbody></table>`;
-        res.send(html); // Send the HTML table
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error fetching programmes data.");
-    }
-});
-
-// Task 5: Create a single-programme page with associated modules
-app.get("/programmes/:id", async function(req, res) {
-    try {
-        const programme = await db.query("SELECT * FROM Programmes WHERE id = ?", [req.params.id]);
-        const modules = await db.query("SELECT m.name FROM Modules m JOIN Programme_Modules pm ON m.code = pm.module WHERE pm.programme = ?", [req.params.id]);
-
-        if (programme.length === 0) {
-            res.status(404).send("Programme not found");
-            return;
-        }
-
-        let html = `<h1>Programme: ${programme[0].name}</h1>`;
-        html += `<h2>Modules:</h2><ul>`;
-        
-        modules.forEach(module => {
-            html += `<li>${module.name}</li>`;
-        });
-        
-        html += `</ul>`;
-        res.send(html); // Send the programme details along with its modules
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error fetching programme data.");
-    }
-});
-
 // Create a route for /goodbye
 app.get("/goodbye", function(req, res) {
     res.send("Goodbye world!");
@@ -151,13 +56,3 @@ app.listen(3000, function(){
     console.log(`Server running at http://127.0.0.1:3000/`);
 });
 
-const express = require("express");
-const app = express();
-
-// Use the Pug templating engine
-app.set('view engine', 'pug');
-app.set('views', './app/views');
-
-app.get("/", function(req, res) {
-    res.render("index", { 'title': 'My index page', 'heading': 'My heading' });
-});
