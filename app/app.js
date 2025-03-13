3dc06cbdf60166d88bedbc472dc9592c31af7458
Mohammad Reza Ahmadi <rezaahmady752@gamil.com>
2025-03-13 15:09:13 +0000

    database

--- app/app.js
+++ app/app.js
@@ -7,12 +7,12 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+
 // Use the Pug templating engine
 app.set('view engine', 'pug');
 app.set('views', './app/views');
-
-// Get the functions in the db.js file to use
-const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
@@ -20,14 +20,94 @@ app.get("/", function(req, res) {
 });
 
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    sql = 'SELECT * FROM test_table';
+app.get("/roeride-form1", function(req, res) {
+    var sql = 'SELECT * FROM Drivers';
+
+    var output = '<table border="1px">';
+    output += '<tr><th>Driver ID</th><th>Name</th></tr>';
+
     db.query(sql).then(results => {
-        console.log(results);
-        res.send(results);
+        for (var row of results) {
+            output += '<tr>';
+            output += '<td>' + row.code + '</td>';
+            output += '<td><a href="./reoride-form1/' + row.code + '">' + row.name + '</a></td>';
+            output += '</tr>';
+        }
+        output += '</table>';
+        res.send(output);
+    }).catch(err => {
+        console.error(err);
+        res.send("Error fetching drivers list.");
     });
 });
 
+
+app.get("/drivers", function(req, res) {
+    sql = 'SELECT * FROM Drivers';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+});
+
+app.get("/roeride/:id", function(req, res) {
+    var rideId = req.params.id;
+    console.log(rideId);
+
+    var rideSql = `
+        SELECT r.id, d.name AS driver, b.name AS booking_type, u.name AS user, 
+               r.start_location, r.end_location, r.ride_purpose
+        FROM Rides r
+        JOIN Drivers d ON r.driver_code = d.code
+        JOIN Booking b ON r.booking_id = b.id
+        JOIN Users u ON r.user_id = u.id
+        WHERE r.id = ?`;
+
+    db.query(rideSql, [rideId]).then(results => {
+        if (results.length === 0) {
+            res.send("No ride found with ID: " + rideId);
+            return;
+        }
+
+        var output = '';
+        output += '<div><b>Ride ID:</b> ' + results[0].id + '</div>';
+        output += '<div><b>Driver:</b> ' + results[0].driver + '</div>';
+        output += '<div><b>User:</b> ' + results[0].user + '</div>';
+        output += '<div><b>Booking Type:</b> ' + results[0].booking_type + '</div>';
+        output += '<div><b>Start Location:</b> ' + results[0].start_location + '</div>';
+        output += '<div><b>End Location:</b> ' + results[0].end_location + '</div>';
+        output += '<div><b>Ride Purpose:</b> ' + results[0].ride_purpose + '</div>';
+
+        res.send(output);
+    }).catch(err => {
+        console.error(err);
+        res.send("Error fetching ride details.");
+    });
+});
+
+
+        app.get("/roeride", function(req, res) {
+            var sql = 'SELECT * FROM Rides';
+            var output = '<table border="1px">';
+            output += '<tr><th>Ride ID</th><th>Start Location</th><th>End Location</th><th>Ride Purpose</th><th>Booking Type</th></tr>';
+        
+            db.query(sql).then(results => {
+                for (var row of results) {
+                    output += '<tr>';
+                    output += '<td><a href="./roeride/' + row.id + '">' + row.id + '</a></td>';
+                    output += '<td>' + row.start_location + '</td>';
+                    output += '<td>' + row.end_location + '</td>';
+                    output += '<td>' + row.ride_purpose + '</td>';
+                    output += '<td>' + row.booking_id + '</td>';
+                    output += '</tr>';
+                }
+                output += '</table>';
+                res.send(output);
+            }).catch(error => {
+                res.status(500).send("Database query failed: " + error.message);
+            });
+        });
+        
 // Task 1: Provide JSON output for all students
 app.get("/students", async function(req, res) {
     try {
--- sd2-db.sql
+++ sd2-db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 30, 2022 at 09:54 AM
--- Server version: 8.0.24
--- PHP Version: 7.4.20
+-- Generation Time: Mar 13, 2025 at 02:15 PM
+-- Server version: 9.2.0
+-- PHP Version: 8.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,41 +24,141 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_table`
+-- Table structure for table `Booking`
 --
 
-CREATE TABLE `test_table` (
-  `id` int NOT NULL,
-  `name` varchar(512) NOT NULL
+CREATE TABLE `Booking` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `test_table`
+-- Dumping data for table `Booking`
 --
 
-INSERT INTO `test_table` (`id`, `name`) VALUES
-(1, 'Lisa'),
-(2, 'Kimia');
+INSERT INTO `Booking` (`id`, `name`) VALUES
+('BKG001', 'Single Ride'),
+('BKG002', 'Round Trip'),
+('BKG003', 'Monthly Pass'),
+('BKG004', 'VIP Service');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Drivers`
+--
+
+CREATE TABLE `Drivers` (
+  `code` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Drivers`
+--
+
+INSERT INTO `Drivers` (`code`, `name`) VALUES
+('DRV001', 'Ali Khan'),
+('DRV002', 'John Smith'),
+('DRV003', 'Emily Davis'),
+('DRV004', 'Michael Brown'),
+('DRV005', 'Sophia Patel'),
+('DRV006', 'Omar Hassan'),
+('DRV007', 'Aisha Rahman'),
+('DRV008', 'James Lee'),
+('DRV009', 'Emma Wilson');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rides`
+--
+
+CREATE TABLE `Rides` (
+  `id` varchar(8) NOT NULL,
+  `booking_id` varchar(10) DEFAULT NULL,
+  `driver_code` varchar(10) DEFAULT NULL,
+  `user_id` varchar(10) DEFAULT NULL,
+  `start_location` varchar(50) DEFAULT NULL,
+  `end_location` varchar(50) DEFAULT NULL,
+  `ride_purpose` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Rides`
+--
+
+INSERT INTO `Rides` (`id`, `booking_id`, `driver_code`, `user_id`, `start_location`, `end_location`, `ride_purpose`) VALUES
+('RID001', 'BKG002', 'DRV002', 'USR005', 'Subway Station', 'City Center', 'Business Trip'),
+('RID002', 'BKG004', 'DRV005', 'USR006', 'Hotel Lobby', 'Grand Theatre', 'Leisure Travel'),
+('RID003', 'BKG001', 'DRV007', 'USR003', 'Residential Area', 'Shopping Mall', 'Grocery Shopping'),
+('RID004', 'BKG003', 'DRV009', 'USR001', 'Train Terminal', 'Business District', 'Work Commute');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `name`) VALUES
+('USR001', 'David Parker'),
+('USR002', 'Sarah Connor'),
+('USR003', 'Robert Johnson'),
+('USR004', 'Linda Nguyen'),
+('USR005', 'Carlos Mendes'),
+('USR006', 'Hina Malik');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `test_table`
+-- Indexes for table `Booking`
 --
-ALTER TABLE `test_table`
+ALTER TABLE `Booking`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `Drivers`
+--
+ALTER TABLE `Drivers`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Indexes for table `Rides`
+--
+ALTER TABLE `Rides`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `booking_id` (`booking_id`),
+  ADD KEY `driver_code` (`driver_code`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `test_table`
+-- Constraints for table `Rides`
 --
-ALTER TABLE `test_table`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `Rides`
+  ADD CONSTRAINT `rides_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `Booking` (`id`),
+  ADD CONSTRAINT `rides_ibfk_2` FOREIGN KEY (`driver_code`) REFERENCES `Drivers` (`code`),
+  ADD CONSTRAINT `rides_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
